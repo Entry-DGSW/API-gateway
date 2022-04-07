700f633cc4b5df9333f85b8b55ffa7f6ab651288
krung2 <jungbin4337@dgsw.hs.kr>
2022-04-08 08:49:16 +0900

    Feat: api-gateway route

--- src/main/java/com/example/gateway/ApiGatewayApplication.java
+++ src/main/java/com/example/gateway/ApiGatewayApplication.java
@@ -2,11 +2,16 @@ package com.example.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
+        System.setProperties("reactor.netty.http.server.accessLogEnabled", "true");
         SpringApplication.run(ApiGatewayApplication.class, args);
     }
 
