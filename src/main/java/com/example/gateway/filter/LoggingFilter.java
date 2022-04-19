7e7d9dbec469d1bc61715a6d3e40afe8a6247db2
krung2 <jungbin4337@dgsw.hs.kr>
2022-04-19 15:10:30 +0900

    Feat: add global loggin filter

---
+++ src/main/java/com/example/gateway/filter/LoggingFilter.java
@@ -0,0 +1,43 @@
+package com.example.gateway.filter;
+
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Mono;
+
+@Component
+@Slf4j
+public class LoggingFilter extends AbstractGatewayFilterFactory<LoggingFilter.Config> {
+
+    public LoggingFilter() {
+        super(Config.class);
+    }
+
+    @Override
+    public GatewayFilter apply(Config config) {
+        return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            ServerHttpResponse response = exchange.getResponse();
+
+            if (config.isPreLogger()) {
+                log.info("Loggin start : request id -> {}", request.getId());
+            }
+
+            return chain.filter(exchange).then(Mono.fromRunnable(() -> {
+                if (config.isPostLogger()) {
+                    log.info("Logging Response status : response status -> {}", response.getStatusCode());
+                }
+            }));
+        });
+    }
+
+    @Data
+    public static class Config {
+        private boolean preLogger;
+        private boolean postLogger;
+    }
+}
